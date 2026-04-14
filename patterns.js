a2aa8b0e944f991ef7bd9da9ea7c1a2921b67c17
Natan L <lifsn608@hsd.k12.or.us>
2026-04-14 07:17:02 -0700

    relocated REL's to new file

--- patterns.js
+++ patterns.js
@@ -1,14 +1,6 @@
-// patterns.js (host this on GitHub)
 window.PATTERN_DATA = {
-  glider: `
-bo$2bo$3o!
-`,
-
-  blinker: `
-3o!
-`,
-
-  switchengine: `
-2o$2o2$3bo$3bo$4b3o!
-`
+  glider: "bo$2bo$3o!",
+  blinker: "3o!",
+  switchengine: "2o$2o2$3bo$3bo$4b3o!"
+  
 };
